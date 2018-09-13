d2cf68811306f17d0cce3666a98e8d52375a58a6
Clara Bell <athenaolson@Athenas-MacBook-Pro.local>
2018-09-13 11:45:34 -0400

    commit 1

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    console.log("ready!");
+
+
+
+      // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBJDWSWFLv_WAsRu23CKCXNLVqw41LJCvc",
+    authDomain: "trainschedule-ce823.firebaseapp.com",
+    databaseURL: "https://trainschedule-ce823.firebaseio.com",
+    projectId: "trainschedule-ce823",
+    storageBucket: "trainschedule-ce823.appspot.com",
+    messagingSenderId: "628946980757"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    });
